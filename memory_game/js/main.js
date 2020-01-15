4fc559259347deb7d1207bf760065e6a582aa550
sarahtsige <sarahtsige@sarahtsiges-MacBook-Air.local>
2020-01-14 20:19:07 -0600

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,20 @@
+console.log("Up and running!")
+const cards = ["queen","queen","king","king"];
+const cardsInPlay = [];
+var cardOne;
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+var cardTwo;
+cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped "+ cardTwo);
+
+if (cardsInPlay.length === 2) {
+if (cardsInPlay[0] === cardsInPlay[1])	{
+	alert("You found a match!"); 
+}
+else {
+	alert("Sorry, try again.");
+}
+}
